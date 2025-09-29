b54535c0f24335ba3cadf73903f3c8639f07d565
Dat Mai <datmtse161965@fpt.edu.vn>
2025-09-29 10:36:57 +0700

    auth page implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,27 @@ import {
   UserButton,
   SignInButton,
 } from "@clerk/clerk-react";
+import { Navigate, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import AuthPage from "./pages/AuthPage";
+import "../src/styles/auth.css";
 const App = () => {
   return (
-    <header>
-      <SignedOut>
-        <SignInButton mode="modal" />
-      </SignedOut>
+    <>
       <SignedIn>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<Navigate to="/" replace />} />
+        </Routes>
         <UserButton />
       </SignedIn>
-    </header>
+      <SignedOut>
+        <Routes>
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="*" element={<Navigate to="/auth" replace />} />
+        </Routes>
+      </SignedOut>
+    </>
   );
 };
 
