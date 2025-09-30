82bb6ecbcceb80af38e93c9672e166b74c60422b
Dat Mai <datmtse161965@fpt.edu.vn>
2025-09-30 11:45:35 +0700

    video call feature added

--- frontend/src/pages/CallPage.jsx
+++ frontend/src/pages/CallPage.jsx
@@ -1,5 +1,105 @@
+import {
+  StreamVideo,
+  StreamVideoClient,
+  StreamCall,
+  CallControls,
+  SpeakerLayout,
+  StreamTheme,
+  CallingState,
+  useCallStateHooks,
+} from "@stream-io/video-react-sdk";
+import { useNavigate, useParams } from "react-router";
+import { useUser } from "@clerk/clerk-react";
+import { useQuery } from "@tanstack/react-query";
+import { getStreamToken } from "../lib/api";
+import { toast } from "react-hot-toast";
+
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+import { useEffect, useState } from "react";
+
+const STREAM_API_KEY = import.meta.env.VITE_STREAM_API_KEY;
+
 const CallPage = () => {
-  return <div>CallPage</div>;
+  const { id: callId } = useParams();
+  const { user, isLoaded } = useUser();
+  const [client, setClient] = useState(null);
+  const [call, setCall] = useState(null);
+  const [isConnecting, setIsConnecting] = useState(true);
+
+  const { data: tokenData } = useQuery({
+    queryKey: ["streamToken"],
+    queryFn: getStreamToken,
+    enabled: !!user,
+  });
+  useEffect(() => {
+    const initCall = async () => {
+      if (!tokenData?.token || !user || !callId) return;
+
+      try {
+        const videoClient = new StreamVideoClient({
+          apiKey: STREAM_API_KEY,
+          token: tokenData.token,
+          user: {
+            id: user.id,
+            name: user.fullName,
+            image: user.imageUrl,
+          },
+        });
+        const callInstance = videoClient.call("default", callId);
+        await callInstance.join({ create: true });
+        setClient(videoClient);
+        setCall(callInstance);
+      } catch (error) {
+        console.error("Error initializing call:", error);
+        toast.error("Error initializing call. Please try again.");
+      } finally {
+        setIsConnecting(false);
+      }
+    };
+    initCall();
+  }, [tokenData, user, callId]);
+
+  if (isConnecting || !isLoaded) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        Connecting to call...
+      </div>
+    );
+  }
+  return (
+    <div className="h-screen flex flex-col items-center justify-center bg-gray-100">
+      <div className="relative w-full max-w-4xl mx-auto">
+        {client && call ? (
+          <StreamVideo client={client}>
+            <StreamCall call={call}>
+              <CallContent />
+            </StreamCall>
+          </StreamVideo>
+        ) : (
+          <div className="flex text-center justify-center h-full">
+            <p>Could not initialize call. Refresh or Try again later.</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+const CallContent = () => {
+  const { useCallCallingState } = useCallStateHooks();
+  const callingState = useCallCallingState();
+  const navigate = useNavigate();
+
+  if (callingState === CallingState.LEFT) {
+    return navigate("/");
+  }
+
+  return (
+    <StreamTheme>
+      <SpeakerLayout />
+      <CallControls />
+    </StreamTheme>
+  );
 };
 
 export default CallPage;
